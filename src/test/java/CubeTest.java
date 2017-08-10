11e91eda80c815bdf91cd1eb83c6c572d56f0e8e
Guest User <Guest@Epicodus-5D.local>
2017-08-10 08:14:01 -0700

    fail test to determine surface area of cube

--- src/test/java/CubeTest.java
+++ src/test/java/CubeTest.java
@@ -28,4 +28,10 @@ public class CubeTest {
         assertEquals(27000, testCube.volume());
     }
 
+    @Test
+    public void surfaceArea_determinesTheSurfaceAreaOfACube_5400() throws Exception {
+        Rectangle testRectangle = new Rectangle(30, 30);
+        Cube testCube = new Cube(testRectangle);
+        assertEquals(5400, testCube.surfaceArea());
+    }
 }
