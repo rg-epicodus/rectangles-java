91161548187aa2ada561fb053ddb3279d8d89c2e
Guest User <Guest@Epicodus-5D.local>
2017-08-10 07:43:16 -0700

    add Cube class and fail test to instantiate instance of Cube

---
+++ src/main/java/Cube.java
@@ -0,0 +1,13 @@
+
+/**
+ * Created by Guest on 8/10/17.
+ */
+public class Cube {
+    private Rectangle face;
+
+    public Cube(Rectangle rectangle) {
+        face = rectangle;
+    }
+
+
+}
---
+++ src/test/java/CubeTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/10/17.
+ */
+public class CubeTest {
+
+    @Test
+    public void newCube_instantiatesCorrectly() throws Exception {
+        Rectangle testRectangle = new Rectangle(30, 30);
+        Cube testCube = new Cube(testRectangle);
+        assertEquals(false, testCube instanceof Cube);
+    }
+
+
+}
