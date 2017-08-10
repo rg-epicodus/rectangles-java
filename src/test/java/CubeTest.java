87810938b7b783eb2b24099714007bc358beb38c
Guest User <Guest@Epicodus-5D.local>
2017-08-10 07:54:24 -0700

    fail test to determine volume of cube

--- src/test/java/CubeTest.java
+++ src/test/java/CubeTest.java
@@ -21,4 +21,11 @@ public class CubeTest {
         assertEquals(testRectangle, testCube.getFace());
     }
 
+    @Test
+    public void volume_determinesTheVolumeOfTheCube_27000() throws Exception {
+        Rectangle testRectangle = new Rectangle(30,30);
+        Cube testCube = new Cube(testRectangle);
+        assertEquals(27000, testCube.volume());
+    }
+
 }
